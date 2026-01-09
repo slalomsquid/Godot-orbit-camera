6b5b25707d3467a11b755c6b267c66c2aec299df
SlalomSquid <slalomsquid@outlook.com>
2026-01-09 15:00:43 +0800

    Ported to 4.5.1
    Changed routines for moving the camera and starting variables to work with modern versions of Godot

--- OrbitCamera.gd
+++ OrbitCamera.gd
@@ -1,29 +1,30 @@
-extends Spatial
+extends Node3D
 
 # Simple orbit camera rig control script
 
 # control variables
-export var maxPitchDeg : float = 45
-export var minPitchDeg : float = -45
-export var maxZoom : float = 20
-export var minZoom : float = 4
-export var zoomStep : float = 2
-export var zoomYStep : float = 0.15
-export var verticalSensitivity : float = 0.002
-export var horizontalSensitivity : float = 0.002
-export var camYOffset : float = 4.0
-export var camLerpSpeed : float = 16.0
-export(NodePath) onready var _camTarget = get_node(_camTarget) as Spatial
+@export var maxPitchDeg : float = 45
+@export var minPitchDeg : float = -45
+@export var maxZoom : float = 50
+@export var minZoom : float = 4
+@export var zoomStep : float = 2
+@export var zoomYStep : float = 0.15
+@export var verticalSensitivity : float = 0.002
+@export var horizontalSensitivity : float = 0.002
+@export var camYOffset : float = 50.0
+@export var camLerpSpeed : float = 16.0
+@export var _curYoffset : float = 10
+@export var _curZoom : float = 10
+
+#@export var _curYoffset : float = camYOffset
 
 # private variables
-onready var _springArm : SpringArm = get_node("SpringArm")
-onready var _curZoom : float = maxZoom
+@onready var _camTarget = $SpringArm3D/Camera3D
+@onready var _springArm : SpringArm3D = get_node("SpringArm3D")
 
-var _curYoffset : float = camYOffset
-
+# No need to be top layer
 func _ready() -> void:
-	# make sure rig transform is independant
-	set_as_toplevel(true)
+	_springArm.spring_length = _curZoom
 
 func _input(event) -> void:
 	if event is InputEventMouseMotion:
@@ -32,23 +33,23 @@ func _input(event) -> void:
 		rotation.y = wrapf(rotation.y,0.0,TAU)
 		
 		rotation.x -= event.relative.y * verticalSensitivity
-		rotation.x = clamp(rotation.x, deg2rad(minPitchDeg), deg2rad(maxPitchDeg))
+		rotation.x = clamp(rotation.x, deg_to_rad(minPitchDeg), deg_to_rad(maxPitchDeg))
 		
 	if event is InputEventMouseButton:
 		# change zoom level on mouse wheel rotation
 		# this could be refactored to be based on an input action as well
 		if event.is_pressed():
-			if event.button_index == BUTTON_WHEEL_UP and _curZoom > minZoom:
+			if event.button_index == MOUSE_BUTTON_WHEEL_UP and _curZoom > minZoom:
 				_curZoom -= zoomStep
 				camYOffset -= zoomYStep
-			if event.button_index == BUTTON_WHEEL_DOWN and _curZoom < maxZoom:
+			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN and _curZoom < maxZoom:
 				_curZoom += zoomStep
 				camYOffset += zoomYStep
+				print(_curZoom)
 
 func _physics_process(delta) -> void:
 	# zoom the camera accordingly
-	_springArm.spring_length = lerp(_springArm.spring_length, _curZoom, delta * camLerpSpeed)
-	
-	# set the position of the rig to follow the target
 	_curYoffset = lerp(_curYoffset, camYOffset, delta * camLerpSpeed)
-	set_translation(_camTarget.global_transform.origin + Vector3(0,_curYoffset,0))
+	_camTarget.position.y = _curYoffset
+	_springArm.spring_length = lerp(_springArm.spring_length, _curZoom, delta * camLerpSpeed)
+	# No need to move rig as it is a child of the player
--- Player.gd
+++ Player.gd
@@ -1,13 +1,16 @@
-extends KinematicBody
+extends CharacterBody3D
 
 # A basic kinematic character controller
 # referenced from https://www.youtube.com/watch?v=UpF7wm0186Q
 
-export(NodePath) onready var cameraRig = get_node(cameraRig) as Spatial
-export(NodePath) onready var characterModel = get_node(characterModel) as Spatial
-export(float) var speed : float = 7.0
-export(float) var jumpStrength : float = 20.0
-export(float) var gravity : float = 50.0
+#@export(NodePath) onready var cameraRig = get_node(cameraRig) as Node3D
+#@export(NodePath) onready var characterModel = get_node(characterModel) as Node3D
+@export var speed: float = 7.0
+@export var jumpStrength: float = 20.0
+@export var gravity: float = 50.0
+
+@onready var characterModel = $Capsule
+@onready var cameraRig = $CameraRig
 
 var _velocity : Vector3 = Vector3.ZERO
 var _snap_vector : Vector3 = Vector3.DOWN
@@ -20,26 +23,25 @@ func _physics_process(delta: float) -> void:
 	moveDirection.z = Input.get_axis("move_forward", "move_back")
 	moveDirection = moveDirection.rotated(Vector3.UP, cameraRig.rotation.y).normalized()
 	
-	# apply movement to velcity
+	# apply horizontal movement to velcity
 	_velocity.x = moveDirection.x * speed
 	_velocity.z = moveDirection.z * speed
 	
-	# apply gravity
-	_velocity.y -= gravity * delta
-	
-	# check if in air or jumping
-	var justLanded : bool = is_on_floor() && _snap_vector == Vector3.ZERO
-	var isJumping : bool = is_on_floor() && Input.is_action_just_pressed("jump")
-	
-	# jump if we are allowed to
-	if isJumping:
-		_velocity.y = jumpStrength
-		_snap_vector = Vector3.ZERO
-	elif justLanded:
-		_snap_vector = Vector3.DOWN
+
+	if is_on_floor():
+		_velocity.y = 0.0
+		if Input.is_action_just_pressed("jump"):
+			_velocity.y = jumpStrength
+	else:
+		_velocity.y -= gravity * delta
 	
 	# finally apply movement
-	_velocity = move_and_slide_with_snap(_velocity, _snap_vector, Vector3.UP, true)
+	set_velocity(_velocity)
+	# TODOConverter3To4 looks that snap in Godot 4 is float, not vector like in Godot 3 - previous value `_snap_vector`
+	set_up_direction(Vector3.UP)
+	set_floor_stop_on_slope_enabled(true)
+	move_and_slide()
+	_velocity = velocity
 	
 	# rotate player model to face movement direction
 	if _velocity.length() > 0.2:
